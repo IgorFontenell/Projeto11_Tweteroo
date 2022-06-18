aae3a6583a437f445b1528daaa823fa67a2a46f2
Igor <igorfontenelletexeira@gmail.com>
2022-06-17 22:16:50 -0300

    Versão Básica Completa

--- back-end/index.js
+++ back-end/index.js
@@ -3,22 +3,59 @@ import cors from "cors";
 
 const server = express();
 server.use(cors());
+server.use(express.json());
 
 const users = [];
 
+const tweets = [];
+
 server.post("/sign-up", (request, response) => {
-    console.log(request);
-    // users.push(
-    //     {
-    //     username: request.body.username,
-    //     avatar: request.body.avatar
-    //     }
-    //  );
-    //  response.send("OK");
+    
+     users.push(
+         {
+         username: request.body.username,
+         avatar: request.body.avatar
+         }
+      );
+      response.send("OK");
+      
 });
 
-server.get("/sign-up", (request, response) => {
-    response.send(users);
+
+
+server.post("/tweets", (request, response) => {
+
+    let image = "";
+
+    for (let i = 0; i < users.length; i++){
+        if (users[i].username === request.body.username) {
+            image = users[i].avatar;
+            break;
+        }
+    }
+    
+    tweets.push(
+        {
+            username: request.body.username,
+            tweet: request.body.tweet,
+            avatar: image
+        });
+        
+        response.send("OK");
 });
+
+server.get("/tweets", (request, response)=> {
+    let last10 = [];
+    let counter = 0;
+    for(let i = tweets.length; i > 0; i--) {
+        last10.push(tweets[i - 1]);
+        counter++;
+        if(counter === 10) {
+            break;
+        }
+    }
+    response.send(last10);
+});
+
 
 server.listen(5000);
